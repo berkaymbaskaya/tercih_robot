b34d83d4930ca09423b75a82764a643343119b66
Berkay Mehmet Başkaya <110475301+berkaymbaskaya@users.noreply.github.com>
2022-09-25 01:02:12 +0300

    Add files via upload

---
+++ aankara.js
@@ -0,0 +1,79 @@
+//harita oluşturma
+
+  var map=L.map('map').setView([39.922336728815566,32.803],10);
+  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  var uni_icon = L.icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/3311/3311565.png',
+    iconSize: [60, 60]
+    })
+
+
+
+
+
+// fltreleme FOnks
+function filtrele(){
+
+var markerLayer = L.layerGroup();
+map.eachLayer(function (layer) {
+    if (layer instanceof L.Marker){
+        map.removeLayer(layer);
+    }
+});
+  // filtelerin alınması
+var bölüm  = document.getElementById('bölüm').value;
+var sıralama = document.getElementById('sıralama').value;
+
+if ( sıralama <0 || sıralama > 500)
+{ alert("Lütfen geçerli bir puan giriniz (0-500) ");
+return false;}
+
+document.getElementById("p1").innerHTML = "Seçtiğiniz filtere uygun olarak yerleşebileceğiniz Üniversiteler Harita üzerinde işaretlenmiştir. Dilerseniz harita üzerinden sembole tıklayarak ilgili program hakkında daha detaylı alabilirsiniz.";
+
+// obje sayısının belirlenmesi
+
+
+
+const keys=Object.keys(data.okullar);
+const keysLength = keys.length;
+
+
+//bölüm==data.okullar[i]["PROGRAM ADI"] 
+
+// ilk for döngüsü ( bölüm adı ve sıralama taraması için)
+for(i=0;i<keysLength;i++)
+{
+
+if(bölüm==data.okullar[i]["PROGRAM ADI"]  && sıralama > data.okullar[i]["EN KÜÇÜK PUAN"])
+// ikinci for döngüsü (enlem boylam taraması için)
+for(j=0;j<21;j++){
+if(data.okullar[i]["ÜNİVERSİTE ADI"]===Üniversiteler[j].ADI)
+{   
+
+var enlem=Üniversiteler[j].x;
+var boylam=Üniversiteler[j].y;
+
+//marker ekleme 
+var m= new L.Marker([enlem,boylam],{icon:uni_icon}).addTo(markerLayer);
+markerLayer.addTo(map);
+
+//popup ve tooltip eklenmesi
+m.bindTooltip(Üniversiteler[j].ADI);
+m.bindPopup(Üniversiteler[j].ADI + "<br>" + "Bölüm= " + data.okullar[i]["PROGRAM ADI"] + "<br>" + "Kontenjan = " +  
+data.okullar[i]["GENEL KONTENJAN"] + "<br>" + "Puan Türü= " + data.okullar[i]["PUAN TÜRÜ"] + "<br>"  + "taban puanı = "  +  data.okullar[i]["EN KÜÇÜK PUAN"] +"<br>"+ 
+"Fakülte = " + data.okullar[i]["MESLEK YÜKSEKOKULU ADI"] + "<br>" + "Adres = " + Üniversiteler[j].ADRES );
+
+map.flyTo([enlem,boylam],11);
+
+}
+}
+
+}
+
+
+
+}
+
---
+++ uniler.js
@@ -0,0 +1,172 @@
+
+  var Üniversiteler= [
+    {
+      "SIRA NO": 1,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ANKARA BİLİM ÜNİVERSİTESİ",
+      "ADRES": "Çamlıca Mahallesi Anadolu Bulvarı No:16A/1 Yenimahalle, Ankara",
+      "x": 39.95247679,
+      "y": 32.77185568
+    },
+    {
+      "SIRA NO": 2,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ANKARA HACI BAYRAM VELİ ÜNİVERSİTESİ",
+      "ADRES": "Yücetepe Mahallesi 85.Cadde No:8 06570 Çankaya / Ankara",
+      "x": 39.91786034,
+      "y": 32.84266763
+    },
+    {
+      "SIRA NO": 3,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ANKARA LOKMAN HEKİM ÜNİVERSİTESİ",
+      "ADRES": "Söğütözü Mahallesi 2179. Cadde No:6 Çankaya / Ankara",
+      "x": 39.9114484,
+      "y": 32.78734873
+    },
+    {
+      "SIRA NO": 4,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ANKARA MEDİPOL ÜNİVERSİTESİ",
+      "ADRES": "Talatpaşa Bulvarı No:2 06050 Altındağ / Ankara",
+      "x": 39.93595189,
+      "y": 32.84538151
+    },
+    {
+      "SIRA NO": 5,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ANKARA MÜZİK ve GÜZEL SANATLAR ÜNİVERSİTESİ",
+      "ADRES": "Turan Güneş Bulvarı Yukarı Dikmen Mahallesi, 648. Cadde, No:4 06550 Oran Çankaya / Ankara",
+      "x": 39.85711648,
+      "y": 32.84551608
+    },
+    {
+      "SIRA NO": 6,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ANKARA SOSYAL BİLİMLER ÜNİVERSİTESİ",
+      "ADRES": "Hükümet Meydanı No:2 06050 Ulus, Altındağ / Ankara",
+      "x": 39.94287142,
+      "y": 32.85642403
+    },
+    {
+      "SIRA NO": 7,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ANKARA ÜNİVERSİTESİ",
+      "ADRES": "Ankara Üniversitesi Rektörlüğü, Dögol Caddesi 06100 Beşevler / Ankara",
+      "x": 39.93562046,
+      "y": 32.83376429
+    },
+    {
+      "SIRA NO": 8,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ATILIM ÜNİVERSİTESİ",
+      "ADRES": "Kızılcaşar Mahallesi, 06830 İncek Gölbaşı / Ankara",
+      "x": 39.81479689,
+      "y": 32.72430654
+    },
+    {
+      "SIRA NO": 9,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "BAŞKENT ÜNİVERSİTESİ",
+      "ADRES": "Bağlıca Kampüsü Fatih Sultan Mahallesi Eskişehir Yolu 18.km TR 06790 Etimesgut / Ankara",
+      "x": 39.88789094,
+      "y": 32.65430106
+    },
+    {
+      "SIRA NO": 10,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ÇANKAYA ÜNİVERSİTESİ",
+      "ADRES": "Eskişehir Yolu 29. Km, Yukarıyurtçu Mahallesi Mimar Sinan Caddesi No:4 06790, Etimesgut / Ankara",
+      "x": 39.81874075,
+      "y": 32.56248778
+    },
+    {
+      "SIRA NO": 11,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "GAZİ ÜNİVERSİTESİ",
+      "ADRES": "Gazi Üniversitesi Rektörlüğü 06500 Teknikokullar / Ankara",
+      "x": 39.93986921,
+      "y": 32.82223032
+    },
+    {
+      "SIRA NO": 12,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "HACETTEPE ÜNİVERSİTESİ",
+      "ADRES": "Hacettepe Üniversitesi Rektörlüğü Sıhhiye / Ankara",
+      "x": 39.93033215,
+      "y": 32.86563161
+    },
+    {
+      "SIRA NO": 13,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "İHSAN DOĞRAMACI BİLKENT ÜNİVERSİTESİ",
+      "ADRES": "Bilkent Üniversitesi 06800 Bilkent / Ankara",
+      "x": 39.87486795,
+      "y": 32.74756381
+    },
+    {
+      "SIRA NO": 14,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "ORTA DOĞU TEKNİK ÜNİVERSİTESİ (ODTÜ)",
+      "ADRES": "Orta Doğu Teknik Üniversitesi, Üniversiteler Mahallesi Dumlupınar Bulvarı No:1 06800 Çankaya / Ankara",
+      "x": 39.89063562,
+      "y": 32.7800881
+    },
+    {
+      "SIRA NO": 15,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "OSTİM TEKNİK ÜNİVERSİTESİ",
+      "ADRES": "100.Yıl Bulvarı OFİM İş Merkezi No:99 Kat:10 06370 OSTİM Yenimahalle / Ankara",
+      "x": 39.96975787,
+      "y": 32.74482495
+    },
+    {
+      "SIRA NO": 16,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "TED ÜNİVERSİTESİ",
+      "ADRES": "TED Üniversitesi, Ziya Gökalp Caddesi No:48 06420 Kolej Çankaya / Ankara",
+      "x": 39.92350013,
+      "y": 32.86169924
+    },
+    {
+      "SIRA NO": 17,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "TOBB EKONOMİ VE TEKNOLOJİ ÜNİVERSİTESİ",
+      "ADRES": "Söğütözü Caddesi No:43 Söğütözü Ankara 06560",
+      "x": 39.92128885,
+      "y": 32.79828934
+    },
+    {
+      "SIRA NO": 18,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "TÜRK HAVA KURUMU ÜNİVERSİTESİ",
+      "ADRES": "Bahçekapı Mahallesi Okul Sokak No:11 Etimesgut / Ankara",
+      "x": 39.94575161,
+      "y": 32.68713764
+    },
+    {
+      "SIRA NO": 19,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "UFUK ÜNİVERSİTESİ",
+      "ADRES": "Ufuk Üniversitesi Rektörlüğü Kızılcaşar Mahallesi İncek Bulvarı 06836 İncek Gölbaşı / Ankara",
+      "x": 39.82670965,
+      "y": 32.72971307
+    },
+    {
+      "SIRA NO": 20,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "YILDIRIM BEYAZIT ÜNİVERSİTESİ",
+      "ADRES": "Ankara Yıldırım Beyazıt Üniversitesi Milli İrade Binası - Ayvalı Mahallesi Gazze Caddesi No:7 Etlik - Keçiören / Ankara",
+      "x": 39.97651274,
+      "y": 32.82340144
+    },
+    {
+      "SIRA NO": 21,
+      "TÜR": "ÜNİVERSİTE",
+      "ADI": "YÜKSEK İHTİSAS ÜNİVERSİTESİ",
+      "ADRES": "Oğuzlar Mahallesi 1377 Sokak No:21 06520 Balgat Ankara",
+      "x": 39.89512627,
+      "y": 32.79868576
+    }
+  ]
+  module.export= {Üniversiteler};
