60e8f9b4f90131c040fb6721d396856ab109dceb
Berkay Mehmet Başkaya <110475301+berkaymbaskaya@users.noreply.github.com>
2022-09-25 09:52:55 +0300

    Update aankara.js

--- aankara.js
+++ aankara.js
@@ -1,6 +1,6 @@
 //harita oluşturma
 
-}
+
   var map=L.map('map').setView([39.922336728815566,32.803],10);
   L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
