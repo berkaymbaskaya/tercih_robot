6944c9b57cd1aebbc252a6ea4a003a413acbc451
Berkay Mehmet Başkaya <110475301+berkaymbaskaya@users.noreply.github.com>
2022-09-25 13:22:03 +0300

    Update aankara.js

--- aankara.js
+++ aankara.js
@@ -1,30 +1,52 @@
+//açılır kapanır sidebar
+
+function onbtn() {
+  document.getElementById("sidebar").style.width="20%";
+  document.getElementById("onbutton").style.visibility="hidden";
+
+
+
+  };
+function offbtn() {
+document.getElementById("sidebar").style.width="0";
+document.getElementById("onbutton").style.visibility="visible";
+
+};
+
+
+  
+
+
+
+
 //harita oluşturma
+var map=L.map('map').setView([39.922336728815566,32.803],10 );
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
+// zoo kontrolü sağ alta alma işlemi
+map.zoomControl.remove();
 
-  var map=L.map('map').setView([39.922336728815566,32.803],10);
-  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-
-  var uni_icon = L.icon({
-    iconUrl: 'https://cdn-icons-png.flaticon.com/512/3311/3311565.png',
-    iconSize: [60, 60]
-    })
-
-
-
-
+L.control.zoom({
+  position: 'bottomright'
+}).addTo(map);
+var uni_icon = L.icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/3311/3311565.png',
+  iconSize: [60, 60]
+  })
+//---------------
 
 // fltreleme FOnks
 function filtrele(){
 
 var markerLayer = L.layerGroup();
 map.eachLayer(function (layer) {
-    if (layer instanceof L.Marker){
-        map.removeLayer(layer);
-    }
+  if (layer instanceof L.Marker){
+      map.removeLayer(layer);
+  }
 });
-  // filtelerin alınması
+// filtelerin alınması
 var bölüm  = document.getElementById('bölüm').value;
 var sıralama = document.getElementById('sıralama').value;
 
@@ -42,7 +64,7 @@ const keys=Object.keys(data.okullar);
 const keysLength = keys.length;
 
 
-//bölüm==data.okullar[i]["PROGRAM ADI"] 
+//bölüm==data.okullar[i]["PROGRAM ADI"]
 
 // ilk for döngüsü ( bölüm adı ve sıralama taraması için)
 for(i=0;i<keysLength;i++)
@@ -52,19 +74,19 @@ if(bölüm==data.okullar[i]["PROGRAM ADI"]  && sıralama > data.okullar[i]["EN K
 // ikinci for döngüsü (enlem boylam taraması için)
 for(j=0;j<21;j++){
 if(data.okullar[i]["ÜNİVERSİTE ADI"]===Üniversiteler[j].ADI)
-{   
+{
 
 var enlem=Üniversiteler[j].x;
 var boylam=Üniversiteler[j].y;
 
-//marker ekleme 
+//marker ekleme
 var m= new L.Marker([enlem,boylam],{icon:uni_icon}).addTo(markerLayer);
 markerLayer.addTo(map);
 
 //popup ve tooltip eklenmesi
 m.bindTooltip(Üniversiteler[j].ADI);
-m.bindPopup(Üniversiteler[j].ADI + "<br>" + "Bölüm= " + data.okullar[i]["PROGRAM ADI"] + "<br>" + "Kontenjan = " +  
-data.okullar[i]["GENEL KONTENJAN"] + "<br>" + "Puan Türü= " + data.okullar[i]["PUAN TÜRÜ"] + "<br>"  + "taban puanı = "  +  data.okullar[i]["EN KÜÇÜK PUAN"] +"<br>"+ 
+m.bindPopup(Üniversiteler[j].ADI + "<br>" + "Bölüm= " + data.okullar[i]["PROGRAM ADI"] + "<br>" + "Kontenjan = " +
+data.okullar[i]["GENEL KONTENJAN"] + "<br>" + "Puan Türü= " + data.okullar[i]["PUAN TÜRÜ"] + "<br>"  + "taban puanı = "  +  data.okullar[i]["EN KÜÇÜK PUAN"] +"<br>"+
 "Fakülte = " + data.okullar[i]["MESLEK YÜKSEKOKULU ADI"] + "<br>" + "Adres = " + Üniversiteler[j].ADRES );
 
 map.flyTo([enlem,boylam],11);
